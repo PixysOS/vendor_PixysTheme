b03943709d32dd0a46750b0f188d07c481416341
harsh-23 <harsh.v23labs@gmail.com>
2019-09-11 16:06:12 +0200

    PixysTheme: accents: Add new accent colors for Q
    Change-Id: I2ce480137e9b6f1946083ce3361fd3d9cdff87c6
    Signed-off-by: Subinsmani <subins.mani@gmail.com>

---
+++ config.mk
@@ -0,0 +1,16 @@
+# Accents
+PRODUCT_PACKAGES += \
+    AccentColorYellowOverlay \
+    AccentColorVioletOverlay \
+    AccentColorTealOverlay \
+    AccentColorRedOverlay \
+    AccentColorQGreenOverlay \
+    AccentColorPinkOverlay \
+    AccentColorLightPurpleOverlay \
+    AccentColorIndigoOverlay \
+    AccentColorFlatPinkOverlay \
+    AccentColorCyanOverlay \
+    AccentColorBlueGrayOverlay
+
+# Call Overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/PixysTheme/overlay
