d7795d795a02cb206f85e03aa60a17b2957cf584
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-01-17 16:10:16 +0100

    ColorBucket: Update all our themes
    - Adapted as per DU backgrounds
    refer: https://github.com/DirtyUnicorns/android_vendor_themes/tree/q10x/backgrounds
    
    Change-Id: I36141fcb8d9871b7debccd0cbc10bd8b0277e536
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

---
+++ config/common.mk
@@ -0,0 +1,122 @@
+# Copyright (C) 2018-19 Superior OS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_BRAND ?= SuperiorOS
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/superior/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/superior/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
+    vendor/superior/prebuilt/common/bin/50-superior.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-superior.sh
+
+ifneq ($(AB_OTA_PARTITIONS),)
+PRODUCT_COPY_FILES += \
+    vendor/superior/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
+    vendor/superior/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
+    vendor/superior/prebuilt/common/bin/backuptool_postinstall.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_postinstall.sh
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.ota.allow_downgrade=true
+endif
+endif
+
+# Copy all Superior-specific init rc files
+$(foreach f,$(wildcard vendor/superior/prebuilt/common/etc/init/*.rc),\
+	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
+
+# Enable Android Beam on all targets
+PRODUCT_COPY_FILES += \
+    vendor/superior/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.sip.voip.xml
+
+# Enable wireless Xbox 360 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Vendor_045e_Product_0719.kl
+
+# Dex preopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI \
+    Launcher3QuickStep
+
+# Don't compile SystemUITests
+EXCLUDE_SYSTEMUI_TESTS := true
+
+#Superior Permissions
+PRODUCT_COPY_FILES += \
+    vendor/superior/config/permissions/privapp-permissions-superior-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-superior.xml \
+    vendor/superior/config/permissions/privapp-permissions-superior.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-superior.xml \
+    vendor/superior/prebuilt/google/etc/sysconfig/pixel_experience_2020.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_experience_2020.xml
+
+# Include LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/superior/overlay/dictionaries
+
+# Do not include art debug targets
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Storage manager
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.storage_manager.enabled=true
+
+# Media
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    media.recorder.show_manufacturer_and_model=true
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/superior/overlay
+
+# Default ringtone/notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Soulful.ogg \
+    ro.config.notification_sound=Pikachu.ogg \
+    ro.config.alarm_alert=Helium.ogg
+
+# Device Overlays
+DEVICE_PACKAGE_OVERLAYS += vendor/superior/overlay/common
+
+#Telephony
+$(call inherit-product, vendor/superior/config/telephony.mk)
+
+# Packages
+include vendor/superior/config/packages.mk
+
+# Custom Overlays
+# Settings
+PRODUCT_PACKAGES += \
+    SystemPitchBlackOverlay \
+    SystemUIPitchBlackOverlay \
+    SystemDarkGrayOverlay \
+    SystemUIDarkGrayOverlay \
+    SystemStyleOverlay \
+    SystemUIStyleOverlay \
+    SystemNightOverlay \
+    SystemUINightOverlay
+
+#versioning
+include vendor/superior/config/version.mk
+
+# Bootanimation
+include vendor/superior/prebuilt/common/bootanimation/bootanimation.mk
+
+#Audio
+include vendor/superior/config/audio.mk
+
+# Superior_props
+$(call inherit-product, vendor/superior/config/superior_props.mk)
