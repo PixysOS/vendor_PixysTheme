7197775d4eb104191ac81e9f7d49c451123a98d6
SuperDroidBond <superdroidbond@yahoo.com>
2020-01-22 22:49:48 +0530

    ColorBucket: Add Choco X & Baked Green
    Change-Id: Ie62908765d3a2ec109f8de5c12d7ed61772f4a23

--- config.mk
+++ config.mk
@@ -63,4 +63,8 @@ PRODUCT_PACKAGES += \
     SystemSolarizedDarkOverlay \
     SystemUISolarizedDarkOverlay \
     SystemMaterialOceanOverlay \
-    SystemUIMaterialOceanOverlay
+    SystemUIMaterialOceanOverlay \
+    SystemBakedGreenOverlay \
+    SystemUIBakedGreenOverlay \
+    SystemChocoXOverlay \
+    SystemUIChocoXOverlay
