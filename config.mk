1c837ffbb1d644663446a7a734ccb5c49da1c886
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-24 15:18:48 +0530

    PixysTheme: Add iOS Blue accent
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: I8ba56c82a3ca465520e379d9e78d0ecd0a69639c

--- config.mk
+++ config.mk
@@ -14,6 +14,7 @@ PRODUCT_PACKAGES += \
     AccentColorBlueGrayOverlay \
     AccentColorBlueGrayOverlay \
     AccentColorBlackOverlay \
+    AccentColoriOSBlueOverlay \
     AccentColorCinnamonOverlay \
     AccentColorOceanOverlay \
     AccentColorOrchidOverlay \
