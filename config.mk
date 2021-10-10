8b084b60dca905fd20d3c7ca6c7086c0d3d12a74
aswin7469 <aswinas@pixysos.com>
2021-10-11 00:59:47 +0530

    PixysTheme: cleanup for snowcone
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Change-Id: Ibb36076a4208495cc275e2beeecbbd5a547ce57b

--- config.mk
+++ config.mk
@@ -1,75 +1,14 @@
 # Inherit from audio config
 $(call inherit-product, vendor/PixysTheme/config/audio.mk)
 
-# Accents
-PRODUCT_PACKAGES += \
-    AccentColorYellowOverlay \
-    AccentColorVioletOverlay \
-    AccentColorTealOverlay \
-    AccentColorRedOverlay \
-    AccentColorQGreenOverlay \
-    AccentColorPinkOverlay \
-    AccentColorLightPurpleOverlay \
-    AccentColorIndigoOverlay \
-    AccentColorFlatPinkOverlay \
-    AccentColorCyanOverlay \
-    AccentColorBlueGrayOverlay \
-    AccentColorBlueGrayOverlay \
-    AccentColorBlackOverlay \
-    AccentColoriOSBlueOverlay \
-    AccentColorCinnamonOverlay \
-    AccentColorOceanOverlay \
-    AccentColorOrchidOverlay \
-    AccentColorSpaceOverlay \
-    AccentColorGreenOverlay \
-    AccentColorPurpleOverlay \
-    AccentColorMintOverlay \
-     AccentColorAmethystOverlay \
-     AccentColorAquamarineOverlay \
-     AccentColorCarbonOverlay \
-     AccentColorPaletteOverlay \
-     AccentColorSandOverlay \
-     AccentColorTangerineOverlay
-
 # Call Overlay
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/PixysTheme/overlay
+#PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/PixysTheme/overlay
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += vendor/PixysTheme/overlay
-
-# NavigationBar Gestural Mode Overlays
-PRODUCT_PACKAGES += \
-    GesturalNavigationOverlayLong \
-    GesturalNavigationOverlayMedium \
-    GesturalNavigationOverlayHidden
+#DEVICE_PACKAGE_OVERLAYS += vendor/PixysTheme/overlay
 
 # Fonts
 include vendor/PixysTheme/fonts.mk
 
 # Icon Shapes
 include vendor/PixysTheme/iconshapes.mk
-
-# Custom Overlays
-# Settings
-PRODUCT_PACKAGES += \
-    SystemRavenBlackOverlay \
-    SystemUIRavenBlackOverlay \
-    SystemDark12Overlay \
-    SystemUIDark12Overlay \
-    SystemDarkGrayOverlay \
-    SystemUIDarkGrayOverlay \
-    SystemStyleOverlay \
-    SystemUIStyleOverlay \
-    SystemNightOverlay \
-    SystemUINightOverlay \
-    SystemSolarizedDarkOverlay \
-    SystemUISolarizedDarkOverlay \
-    SystemMaterialOceanOverlay \
-    SystemUIMaterialOceanOverlay \
-    SystemBakedGreenOverlay \
-    SystemUIBakedGreenOverlay \
-    SystemChocoXOverlay \
-    SystemUIChocoXOverlay \
-    SystemDarkAubergineOverlay \
-    SystemUIDarkAubergineOverlay
-
